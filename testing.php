e7923774fa250749f0a0730d248c78cfeedd1420
sampath <sampath@gmail.com>
2019-01-24 14:27:00 +0530

    firstcommit

---
+++ testing.php
@@ -0,0 +1,28 @@
+
+
+
+
+
+<html>
+<head>
+<title>jQuery after() Examples</title>
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+<script>
+$(document).ready(function()
+{
+$("div:nth-child(2) p").html("8012118212").add("div:nth-child(2) p").attr("href", "https://www.w3schools.com/jquery/");
+});
+</script>
+</head>
+<body>
+  <div class="one">
+<p>9788298138</p>
+</div>
+ <div class="two">
+<p>9788298138</p>
+</div>
+ <div class="three">
+<p>9788298138</p>
+</div>
+</body>
+</html>
